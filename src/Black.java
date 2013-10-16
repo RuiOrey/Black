8ecaf372bcba8d8c35a1afbaf1f24e8fd03640cc
Danijo <Danieljosuemu@gmail.com>
2013-10-16 21:39:46 +0100

    first experiment

--- src/Black.java
+++ src/Black.java
@@ -6,7 +6,7 @@ public class Black
 {
    public static void main( String[] args )
    {
-      System.loadLibrary( Core.NATIVE_LIBRARY_NAME );
+      System.loadLibrary("VERSION");
       Mat mat = Mat.eye( 3, 3, CvType.CV_8UC1 );
       System.out.println( "mat = " + mat.dump() );
    }
