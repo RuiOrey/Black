574cd0e16dcbd7544cf4f0320e3566f7124ff5bc
unknown <rui.albuquerque.dorey@gmail.com>
2013-10-16 21:45:59 +0100

    Fixes wrong instruction.

--- src/Black.java
+++ src/Black.java
@@ -6,7 +6,7 @@ public class Black
 {
    public static void main( String[] args )
    {
-      System.loadLibrary("VERSION");
+	  System.loadLibrary( Core.NATIVE_LIBRARY_NAME );
       Mat mat = Mat.eye( 3, 3, CvType.CV_8UC1 );
       System.out.println( "mat = " + mat.dump() );
    }
